515817548df4ca72cc4774e1e34f6105f74f1faf
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-04 17:23:43 +0300

    Initial Commit

---
+++ Weather Status/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weather Status
+//
+//  Created by Nikhil Bolar on 2/4/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
