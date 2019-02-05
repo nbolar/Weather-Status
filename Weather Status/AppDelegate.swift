8873c02bf2aaf7689a1b81f63547ba499146e10f
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-05 11:20:52 +0300

    Update

--- Weather Status/AppDelegate.swift
+++ Weather Status/AppDelegate.swift
@@ -11,14 +11,26 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    //Variables
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        statusItem.button?.title = "--°"
+        statusItem.button?.action = #selector(AppDelegate.displayPopUp(_:))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @objc func displayPopUp(_ sender: AnyObject?) {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc =  storyboard.instantiateController(withIdentifier: "WeatherVC") as? NSViewController else { return }
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .minY)
     }
 
 
