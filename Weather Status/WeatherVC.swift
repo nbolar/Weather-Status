c95221207d0faf8b3af8dc55fdf6d42463ec284a
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-05 11:22:07 +0300

    Update

---
+++ Weather Status/WeatherVC.swift
@@ -0,0 +1,65 @@
+//
+//  WeatherVC.swift
+//  Weather Status
+//
+//  Created by Nikhil Bolar on 2/4/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Cocoa
+
+
+class WeatherVC: NSViewController {
+
+    @IBOutlet weak var dateLabel: NSTextField!
+    @IBOutlet weak var tempLabel: NSTextField!
+    @IBOutlet weak var locationLabel: NSTextField!
+    @IBOutlet weak var weatherImage: NSImageView!
+    @IBOutlet weak var weatherConditionLabel: NSTextField!
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateLabel.stringValue = "This is a date"
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear() {
+        self.view.layer?.backgroundColor = CGColor.clear
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
+extension WeatherVC: NSCollectionViewDelegate, NSCollectionViewDataSource, NSCollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let forecastItem = collectionView.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "WeatherCell"), for: indexPath)
+        return forecastItem
+    }
+    
+    func numberOfSections(in collectionView: NSCollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
+        return NSSize(width: 125, height: 125)
+    }
+    
+    
+    
+}
+
