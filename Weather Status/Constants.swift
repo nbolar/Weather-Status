1121b9623e33389acbc543174a4a14a4a5033985
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-05 11:26:24 +0300

    Update

---
+++ Weather Status/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Weather Status
+//
+//  Created by Nikhil Bolar on 2/5/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "47c545315eff2221c6346d3ee071083d"
+let API_URL_CURRENT_WEATHER = "https://api.darksky.net/forecast/\(API_KEY)/35,139?units=si"
+
+
